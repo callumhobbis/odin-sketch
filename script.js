cd18fbf7b5ffe6d5ef93c526de5628f3bd1d1b7b
Callum Hobbis <callum.hobbis@gmail.com>
2024-02-14 12:14:14 +1300

    feat: add gap around tiles

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const GRID_WIDTH = 4;
 const BOX_COUNT = GRID_WIDTH ** 2; // pre-compute
 
 const container = document.getElementById("box-container");
+const gap = 8;
 
 for (i = 0; i < GRID_WIDTH; i++) {
   let row = document.createElement("div");
@@ -11,5 +12,12 @@ for (i = 0; i < GRID_WIDTH; i++) {
     box.classList.add("box");
     row.appendChild(box);
   }
+
+  row.style.gap = `${gap}px`;
+  row.style.padding = `0 ${gap}px`;
+
   container.appendChild(row);
 }
+
+container.style.gap = `${gap}px`;
+container.style.padding = `${gap}px 0`;
