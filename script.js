e24f8392aed08417e14aca779878a07626d2dec8
Callum Hobbis <callum.hobbis@gmail.com>
2024-02-14 12:11:37 +1300

    refactor: use stabler grid generation

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 const GRID_WIDTH = 4;
 const BOX_COUNT = GRID_WIDTH ** 2; // pre-compute
 
-function getBoxWidth(container) {
-  const style = getComputedStyle(container);
-  const containerWidth = container.offsetWidth;
-  const containerPadding = parseInt(style.padding, 10);
-  const containerGap = parseInt(style.gap, 10);
-  const availableSpace =
-    containerWidth - containerPadding - containerGap * (GRID_WIDTH - 1);
-  return Math.floor(availableSpace / GRID_WIDTH);
-}
-
 const container = document.getElementById("box-container");
-const boxWidth = getBoxWidth(container);
 
-for (i = 0; i < BOX_COUNT; i++) {
-  let box = document.createElement("div");
-  box.classList.add("box");
-  box.style.width = `${boxWidth - 2}px`;
-  box.style.height = `${boxWidth - 2}px`;
-  container.appendChild(box);
+for (i = 0; i < GRID_WIDTH; i++) {
+  let row = document.createElement("div");
+  row.classList.add("box-row");
+  for (j = 0; j < GRID_WIDTH; j++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    row.appendChild(box);
+  }
+  container.appendChild(row);
 }
